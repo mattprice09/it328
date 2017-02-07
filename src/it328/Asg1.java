54c08fe4f15c9d8cc06ebf7af4cdd17c96caa8be
mattprice09 <mattprice09@rocketmail.com>
2017-02-07 15:04:28 -0600

    Fixed spacing

--- src/it328/Asg1.java
+++ src/it328/Asg1.java
@@ -19,66 +19,66 @@ public class Asg1 {
       System.out.println(String.join(" ", row));
     }
   }
-	
+  
   /**
    * Reads graphs from a given file name
    */
-	public static ArrayList<Graph> readFile(String fname) {
-	  int numNodes;
-	  
-	  // Open file for viewing
-	  Scanner reader = null;
-		try {
+  public static ArrayList<Graph> readFile(String fname) {
+    int numNodes;
+    
+    // Open file for viewing
+    Scanner reader = null;
+    try {
       reader = new Scanner(new File(fname));
     } catch (FileNotFoundException e) {
       System.out.println("> ERROR: Invalid file name");
       System.exit(0);
     }
-		
-		ArrayList<Graph> graphs = new ArrayList<Graph>();
-		
-		// Read in multiple graphs
-		while (reader.hasNextLine()) {
-		  
-		  // first line contains the number of nodes
-	    String line = reader.nextLine();
-	    numNodes = Integer.parseInt(line);
-	    
-	    // 2-D array to represent graph matrix
-	    int[][] matrix = new int[numNodes][numNodes];
-	    
-	    // read each line of the matrix
-	    for(int i = 0; i < matrix.length; i++) {
-	      line = reader.nextLine();
-	      String [] parts = line.split(" ");
-	      for (int j = 0; j < parts.length; j++) {
-	        matrix[i][j] = Integer.parseInt(parts[j]);
-	      }
-	    }
-	    Graph graph = new Graph(matrix);
-	    graphs.add(graph);
-		}
-		return graphs;
-	}
+    
+    ArrayList<Graph> graphs = new ArrayList<Graph>();
+    
+    // Read in multiple graphs
+    while (reader.hasNextLine()) {
+      
+      // first line contains the number of nodes
+      String line = reader.nextLine();
+      numNodes = Integer.parseInt(line);
+      
+      // 2-D array to represent graph matrix
+      int[][] matrix = new int[numNodes][numNodes];
+      
+      // read each line of the matrix
+      for(int i = 0; i < matrix.length; i++) {
+        line = reader.nextLine();
+        String [] parts = line.split(" ");
+        for (int j = 0; j < parts.length; j++) {
+          matrix[i][j] = Integer.parseInt(parts[j]);
+        }
+      }
+      Graph graph = new Graph(matrix);
+      graphs.add(graph);
+    }
+    return graphs;
+  }
 
-	public static void main(String[] args) {
-	  
-	  // Accepts a graphs file name as the first and only arg
-		if (args.length != 1) {
-			System.out.println("Please provide a file name contaiby referencening a graph matrix.");
-			System.exit(0);
-		}
-		String fname = args[0];
-		
-		// read graphs from file
-		ArrayList<Graph> graphs = readFile(fname);
-		
-		// print all graphs to console
-		int i = 0;
-		for (Graph graph : graphs) {
-		  i++;
-		  System.out.println(">>> Graph " + i);
-	    printGraph(graph);
-		}
-	}
+  public static void main(String[] args) {
+    
+    // Accepts a graphs file name as the first and only arg
+    if (args.length != 1) {
+      System.out.println("Please provide a file name contaiby referencening a graph matrix.");
+      System.exit(0);
+    }
+    String fname = args[0];
+    
+    // read graphs from file
+    ArrayList<Graph> graphs = readFile(fname);
+    
+    // print all graphs to console
+    int i = 0;
+    for (Graph graph : graphs) {
+      i++;
+      System.out.println(">>> Graph " + i);
+      printGraph(graph);
+    }
+  }
 }
